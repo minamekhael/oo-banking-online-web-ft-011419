ef28c7ef8fffc29d2d23d7b8a755987eb9219b61
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-31 22:11:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,9 +2,10 @@ class Transfer
   
   attr_accessor :sender, :receiver
   
-  def initialize(sender, receiver, transfer_amount, @status = "pending")
+  def initialize(sender, receiver, transfer_amount)
     @sender = sender
     @receiver = receiver
+    @status = "pending"
     @transfer_amount = 0
   end
 end
